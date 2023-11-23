4aca3ac7fea37d3512e382c9548cbb70ff9c8816
Sconl Peter <sconl@proton.me>
2023-11-23 10:57:27 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById('about').innerHTML = 'I am a passionate and dedicated Biomedical Design Engineer with a love for innovation and problem-solving. I have a degree in Biomedical Engineering from XYZ University and have been working in the field for over 5 years.';
+
+document.getElementById('work').innerHTML = 'I specialize in designing and developing biomedical devices that improve the quality of life for patients around the world. I have worked on projects involving prosthetics, medical imaging devices, and health monitoring systems.';
+
+document.getElementById('contact').innerHTML = 'If you\'d like to get in touch, please send me an email at sconl.peter@example.com.';
